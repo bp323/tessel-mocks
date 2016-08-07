daaf05351afbcac4db996aa2072ee3eaf783e7d6
Eric McNiece <emcniece@gmail.com>
2016-08-07 16:04:00 -0700

    Implementing I2C mock class

--- README.md
+++ README.md
@@ -23,7 +23,100 @@ try {
 }
 ```
 
-From this point on, when running locally the Tessel commands will print statements in the console instead of performing actions on the device.
+From this point on when running locally, the Tessel commands will print statements in the console instead of performing actions on the device.
+
+* To run code on the Tessel: `t2 run index.js`
+* To run code locally: `node index.js`
+
+###Example Comparison
+
+When running on the Tessel, this code will flash LED3 (blue) every 2 seconds:
+
+```js
+setInterval(function(){
+  tessel.led[3].toggle();
+}, 2000);
+```
+
+... but when running locally with `node index.js`, the console prints this:
+
+```sh
+Tessel not found - running mocks instead
+TM | Toggled LED2 on
+TM | Toggled LED2 off
+TM | Toggled LED2 on
+```
+
+###Mocking Responses
+
+Serial communication devices can be mocked by passing additional parameters in any method that supports a callback. These extra parameters do not affect Tessel runtime and will be ignored on the device, but while running locally they will return the values specified.
+
+To do this, pass 2 extra parameters (as if they were the callback values) to the target functions. Take the I2C Transfer method:
+
+```js
+i2c.transfer(txbuf, rxlen, callback);
+```
+
+... and this becomes:
+
+```js
+i2c.transfer(txbuf, rxlen, callback, mockError, mockReturn);
+```
+
+**Example: [BME280](https://github.com/emcniece/tessel-bme280) I2C**
+
+This code prints no error and returns the ID of the BME280 device (which is `60`):
+
+```js
+var slaveAddress = 0x77;
+var readLength = 1;
+var i2c = new tessel.port.A.I2C(slaveAddress)
+
+i2c.transfer(new Buffer([0xD0]), readLength, function (err, rx) {
+  console.log('error returned by I2C Slave: ', err)
+  console.log('buffer returned by I2C slave ('+slaveAddress.toString(16)+'):', rx);
+});
+```
+
+Output:
+
+```sh
+INFO Running index.js...
+error returned by I2C Slave:  null
+buffer returned by I2C slave (77): <Buffer 60>
+```
+
+If this script is run locally with `node index.js`, the callback will not contain data:
+
+```sh
+INFO Running index.js...
+Tessel not found - running mocks instead
+TM | I2C Transfer: �
+error returned by I2C Slave:  [Error]
+buffer returned by I2C slave (77): undefined
+```
+
+Instead, if mock responses are passed in:
+
+```js
+var slaveAddress = 0x77;
+var readLength = 1;
+var i2c = new tessel.port.A.I2C(slaveAddress)
+
+i2c.transfer(new Buffer([0xD0]), readLength, function (err, rx) {
+  console.log('error returned by I2C Slave: ', err)
+  console.log('buffer returned by I2C slave ('+slaveAddress.toString(16)+'):', rx);
+}, 'Test Error', '99');
+```
+
+... then the mock data will be populated in console:
+
+```sh
+Tessel not found - running mocks instead
+TM | I2C Transfer: �
+error returned by I2C Slave:  [Error: Test Error]
+buffer returned by I2C slave (77): 99
+```
 
 ##Todo Mocks
 
@@ -34,6 +127,6 @@ From this point on, when running locally the Tessel commands will print statemen
 - [ ] Ports
 - [ ] UART
 - [ ] SPI
-- [ ] I2C
+- [x] I2C
 - [ ] PWM
 - [ ] Button
--- test/i2c.js
+++ test/i2c.js
@@ -1,18 +1,114 @@
 var assert = require('assert');
 var chai = require('chai');
 var expect = chai.expect;
-require('mocha-sinon')();
+var sinon = require('mocha-sinon')();
 
 var I2C = require('../lib/i2c');
 var i2c;
 
+function newClass(addr){
+  if(!addr) addr = 0x77;
+  return new I2C(addr)
+}
+
 describe('I2C Class', function() {
   beforeEach(function () {
-    i2c = new I2C(0x77);
-    this.sinon.stub(console, 'info');
+    i2c = newClass();
+  });
+
+  describe('when not given an address', function () {
+    it('should throw an error', function () {
+      expect( () => {new I2C} ).to.throw(Error);
+    });
   });
 
   it('should have an "address" property', function() {
     expect(i2c).to.have.property('address');
+  });
+
+  it('should have an "options" property', function () {
+    expect(i2c).to.have.property('options');
+  });
+
+  it('should have an "enabled" property', function () {
+    expect(i2c).to.have.property('enabled');
+  });
+
+  it('should have a "frequency" property', function () {
+    expect(i2c).to.have.property('frequency');
+  });
+
+  it('should have an "baudrate" property', function () {
+    expect(i2c).to.have.property('baudrate');
+  });
+
+  describe('#computeBaud', function () {
+    it('should calculate baud given frequency', function () {
+      expect(i2c.computeBaud(1e5)).to.equal(234);
+    });
+  });
+
+  describe('#send', function () {
+    it('should print to console', function () {
+      this.sinon.stub(console, 'info');
+      i2c = newClass();
+      i2c.send();
+      expect(console.info.calledOnce).to.be.true;
+    });
+
+    describe('when given a callback', function () {
+      describe('when given a mockError', function () {
+        it('should return an error', function () {
+          this.sinon.stub(console, 'info');
+          i2c = newClass();
+          i2c.send('data', (mockErr)=>{
+            expect(mockErr.toString() ).to.equal('Error: test error');
+          }, 'test error');
+        });
+
+      });
+
+      describe('when given a mockReturn', function () {
+        it('should return a value', function () {
+          this.sinon.stub(console, 'info');
+          i2c = newClass();
+          i2c.send('data', (mockErr, mockReturn)=>{
+            expect(mockReturn ).to.equal(42);
+          }, null, 42);
+        });
+      });
+    });
+  });
+
+  describe('#transfer', function () {
+    it('should print to console', function () {
+      this.sinon.stub(console, 'info');
+      i2c = newClass();
+      i2c.transfer();
+      expect(console.info.calledOnce).to.be.true;
+    });
+
+    describe('when given a callback', function () {
+      describe('when given a mockError', function () {
+        it('should return an error', function () {
+          this.sinon.stub(console, 'info');
+          i2c = newClass();
+          i2c.send('data', (mockErr)=>{
+            expect(mockErr.toString() ).to.equal('Error: test error');
+          }, 'test error');
+        });
+
+      });
+
+      describe('when given a mockReturn', function () {
+        it('should return a value', function () {
+          this.sinon.stub(console, 'info');
+          i2c = newClass();
+          i2c.send('data', (mockErr, mockReturn)=>{
+            expect(mockReturn ).to.equal(42);
+          }, null, 42);
+        });
+      });
+    });
   });
 });
